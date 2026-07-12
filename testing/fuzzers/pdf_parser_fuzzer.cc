5cdfd29ccda8b381c72050254b4b4e7f0537a110
Sebastian Werner <s.werner@sebastian-software.de>
2026-07-12 13:42:09 +0200

    test(parser): fuzz public object graph differentially

--- testing/fuzzers/pdf_parser_fuzzer.cc
+++ testing/fuzzers/pdf_parser_fuzzer.cc
@@ -12,6 +12,59 @@
 #include "core/fxcrt/span.h"
 #include "public/fpdfview.h"
 
+namespace {
+
+struct DocumentSnapshot {
+  int page_count;
+  int file_version;
+  bool has_file_version;
+  unsigned long permissions;
+  int security_revision;
+};
+
+DocumentSnapshot SnapshotDocument(FPDF_DOCUMENT document,
+                                  bool use_rust_candidate) {
+  pdfium::rust::ScopedRustParserImplementationForTesting implementation(
+      use_rust_candidate);
+  int file_version = 0;
+  const bool has_file_version =
+      FPDF_GetFileVersion(document, &file_version) != 0;
+  return {
+      .page_count = FPDF_GetPageCount(document),
+      .file_version = file_version,
+      .has_file_version = has_file_version,
+      .permissions = FPDF_GetDocPermissions(document),
+      .security_revision = FPDF_GetSecurityHandlerRevision(document),
+  };
+}
+
+void CompareBoundaryPage(FPDF_DOCUMENT reference,
+                         FPDF_DOCUMENT candidate,
+                         int page_index) {
+  FPDF_PAGE reference_page = nullptr;
+  {
+    pdfium::rust::ScopedRustParserImplementationForTesting implementation(
+        false);
+    reference_page = FPDF_LoadPage(reference, page_index);
+  }
+  FPDF_PAGE candidate_page = nullptr;
+  {
+    pdfium::rust::ScopedRustParserImplementationForTesting implementation(true);
+    candidate_page = FPDF_LoadPage(candidate, page_index);
+  }
+  CHECK_EQ(!!reference_page, !!candidate_page);
+  if (reference_page) {
+    CHECK_EQ(FPDF_GetPageWidthF(reference_page),
+             FPDF_GetPageWidthF(candidate_page));
+    CHECK_EQ(FPDF_GetPageHeightF(reference_page),
+             FPDF_GetPageHeightF(candidate_page));
+  }
+  FPDF_ClosePage(reference_page);
+  FPDF_ClosePage(candidate_page);
+}
+
+}  // namespace
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static constexpr size_t kMaxDataSize = 1024 * 1024;
   if (size > kMaxDataSize) {
@@ -42,9 +95,44 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     }
   }
 
-  FPDF_DOCUMENT document = FPDF_LoadMemDocument64(data, size, nullptr);
-  if (document) {
-    FPDF_CloseDocument(document);
+  FPDF_DOCUMENT reference_document = nullptr;
+  unsigned long reference_error = 0;
+  {
+    pdfium::rust::ScopedRustParserImplementationForTesting implementation(
+        false);
+    reference_document = FPDF_LoadMemDocument64(data, size, nullptr);
+    reference_error = FPDF_GetLastError();
   }
+  FPDF_DOCUMENT candidate_document = nullptr;
+  unsigned long candidate_error = 0;
+  {
+    pdfium::rust::ScopedRustParserImplementationForTesting implementation(true);
+    candidate_document = FPDF_LoadMemDocument64(data, size, nullptr);
+    candidate_error = FPDF_GetLastError();
+  }
+  CHECK_EQ(!!reference_document, !!candidate_document);
+  CHECK_EQ(reference_error, candidate_error);
+  if (reference_document) {
+    const DocumentSnapshot reference_snapshot =
+        SnapshotDocument(reference_document, false);
+    const DocumentSnapshot candidate_snapshot =
+        SnapshotDocument(candidate_document, true);
+    CHECK_EQ(reference_snapshot.page_count, candidate_snapshot.page_count);
+    CHECK_EQ(reference_snapshot.file_version, candidate_snapshot.file_version);
+    CHECK_EQ(reference_snapshot.has_file_version,
+             candidate_snapshot.has_file_version);
+    CHECK_EQ(reference_snapshot.permissions, candidate_snapshot.permissions);
+    CHECK_EQ(reference_snapshot.security_revision,
+             candidate_snapshot.security_revision);
+    if (reference_snapshot.page_count > 0) {
+      CompareBoundaryPage(reference_document, candidate_document, 0);
+      if (reference_snapshot.page_count > 1) {
+        CompareBoundaryPage(reference_document, candidate_document,
+                            reference_snapshot.page_count - 1);
+      }
+    }
+  }
+  FPDF_CloseDocument(reference_document);
+  FPDF_CloseDocument(candidate_document);
   return 0;
 }
